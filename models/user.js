9b1b33a2387c8d0bc0342999e97e05ca09d90e47
pranasyabhimanadham <135666933+pranasyabhimanadham@users.noreply.github.com>
2024-04-19 01:08:55 -0400

    Your commit message here

--- models/user.js
+++ models/user.js
@@ -1,3 +1,12 @@
+/****************************************************************************** 
+ITE5315 – Project 
+I declare that this assignment is my own work in accordance with Humber Academic 
+Policy. 
+No part of this assignment has been copied manually or electronically from any other 
+source 
+(including web sites) or distributed to other students. 
+Name: Sai Pranasya Student ID: N01582877 Date: 07/04/2024
+******************************************************************************/
 // load mongoose since we need it to define a model
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
