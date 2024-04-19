adbfc744d5fcaead82c1bccb0178274017d4c0f3
pranasyabhimanadham <135666933+pranasyabhimanadham@users.noreply.github.com>
2024-04-19 17:11:29 -0400

    Your commit message here

--- app.js
+++ app.js
@@ -407,10 +407,9 @@ database
     );
     
 // Start the server
-const server= app.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
-module.exports = app;
   })
   .catch((error) => {
     console.error("Error initializing app:", error.message);
